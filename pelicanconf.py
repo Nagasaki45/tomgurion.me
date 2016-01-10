dc89c19a2eec50cf152a71d8601f7bcf25c80ae2
Tom Gurion <nagasaki45@gmail.com>
2016-01-10 22:17:20 +0200

    Remove unused configurations. Fix #5

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,24 +24,6 @@ DEFAULT_LANG = 'en'
 
 ARTICLE_ORDER_BY = 'order'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = False
 DEFAULT_DATE = 'fs'  # Get date from file creation time if missing
 
 STATIC_PATHS = ['images', 'pdfs', 'extra/favicon.ico']
